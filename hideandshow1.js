d093774e85992722e0f663bcf7e3aa2680d9cb69
Nurkholis <108184877+Nurkholis0@users.noreply.github.com>
2023-09-17 00:39:58 +0700

    Add files via upload

---
+++ hideandshow1.js
@@ -0,0 +1,560 @@
+function q1function() {
+  var q1 = document.getElementById("q1more");
+  if (q1.style.display === "block") {
+    q1.style.display = "none";
+  } else {
+    q1.style.display = "block";
+  }
+}
+function q2function() {
+  var q2 = document.getElementById("q2more");
+  if (q2.style.display === "block") {
+    q2.style.display = "none";
+  } else {
+    q2.style.display = "block";
+  }
+}
+function q3function() {
+  var q3 = document.getElementById("q3more");
+  if (q3.style.display === "block") {
+    q3.style.display = "none";
+  } else {
+    q3.style.display = "block";
+  }
+}
+function q4function() {
+  var q4 = document.getElementById("q4more");
+  if (q4.style.display === "block") {
+    q4.style.display = "none";
+  } else {
+    q4.style.display = "block";
+  }
+}
+function q5function() {
+  var q5 = document.getElementById("q5more");
+  if (q5.style.display === "block") {
+    q5.style.display = "none";
+  } else {
+    q5.style.display = "block";
+  }
+}
+function q6function() {
+  var q6 = document.getElementById("q6more");
+  if (q6.style.display === "block") {
+    q6.style.display = "none";
+  } else {
+    q6.style.display = "block";
+  }
+}
+function q7function() {
+  var q7 = document.getElementById("q7more");
+  if (q7.style.display === "block") {
+    q7.style.display = "none";
+  } else {
+    q7.style.display = "block";
+  }
+}
+function q8function() {
+  var q8 = document.getElementById("q8more");
+  if (q8.style.display === "block") {
+    q8.style.display = "none";
+  } else {
+    q8.style.display = "block";
+  }
+}
+function q9function() {
+  var q9 = document.getElementById("q9more");
+  if (q9.style.display === "block") {
+    q9.style.display = "none";
+  } else {
+    q9.style.display = "block";
+  }
+}
+function q10function() {
+  var q10 = document.getElementById("q10more");
+  if (q10.style.display === "block") {
+    q10.style.display = "none";
+  } else {
+    q10.style.display = "block";
+  }
+}
+function q11function() {
+  var q11 = document.getElementById("q11more");
+  if (q11.style.display === "block") {
+    q11.style.display = "none";
+  } else {
+    q11.style.display = "block";
+  }
+}
+function q12function() {
+  var q12 = document.getElementById("q12more");
+  if (q12.style.display === "block") {
+    q12.style.display = "none";
+  } else {
+    q12.style.display = "block";
+  }
+}
+function q13function() {
+  var q13 = document.getElementById("q13more");
+  if (q13.style.display === "block") {
+    q13.style.display = "none";
+  } else {
+    q13.style.display = "block";
+  }
+}
+function q14function() {
+  var q14 = document.getElementById("q14more");
+  if (q14.style.display === "block") {
+    q14.style.display = "none";
+  } else {
+    q14.style.display = "block";
+  }
+}
+function q15function() {
+  var q15 = document.getElementById("q15more");
+  if (q15.style.display === "block") {
+    q15.style.display = "none";
+  } else {
+    q15.style.display = "block";
+  }
+}
+function q16function() {
+  var q16 = document.getElementById("q16more");
+  if (q16.style.display === "block") {
+    q16.style.display = "none";
+  } else {
+    q16.style.display = "block";
+  }
+}
+function q17function() {
+  var q17 = document.getElementById("q17more");
+  if (q17.style.display === "block") {
+    q17.style.display = "none";
+  } else {
+    q17.style.display = "block";
+  }
+}
+function q18function() {
+  var q18 = document.getElementById("q18more");
+  if (q18.style.display === "block") {
+    q18.style.display = "none";
+  } else {
+    q18.style.display = "block";
+  }
+}
+function q19function() {
+  var q19 = document.getElementById("q19more");
+  if (q19.style.display === "block") {
+    q19.style.display = "none";
+  } else {
+    q19.style.display = "block";
+  }
+}
+function q20function() {
+  var q20 = document.getElementById("q20more");
+  if (q20.style.display === "block") {
+    q20.style.display = "none";
+  } else {
+    q20.style.display = "block";
+  }
+}
+function q21function() {
+  var q21 = document.getElementById("q21more");
+  if (q21.style.display === "block") {
+    q21.style.display = "none";
+  } else {
+    q21.style.display = "block";
+  }
+}
+function q22function() {
+  var q22 = document.getElementById("q22more");
+  if (q22.style.display === "block") {
+    q22.style.display = "none";
+  } else {
+    q22.style.display = "block";
+  }
+}
+function q23function() {
+  var q23 = document.getElementById("q23more");
+  if (q23.style.display === "block") {
+    q23.style.display = "none";
+  } else {
+    q23.style.display = "block";
+  }
+}
+function q24function() {
+  var q24 = document.getElementById("q24more");
+  if (q24.style.display === "block") {
+    q24.style.display = "none";
+  } else {
+    q24.style.display = "block";
+  }
+}
+function q25function() {
+  var q25 = document.getElementById("q25more");
+  if (q25.style.display === "block") {
+    q25.style.display = "none";
+  } else {
+    q25.style.display = "block";
+  }
+}
+function q26function() {
+  var q26 = document.getElementById("q26more");
+  if (q26.style.display === "block") {
+    q26.style.display = "none";
+  } else {
+    q26.style.display = "block";
+  }
+}
+function q27function() {
+  var q27 = document.getElementById("q27more");
+  if (q27.style.display === "block") {
+    q27.style.display = "none";
+  } else {
+    q27.style.display = "block";
+  }
+}
+function q28function() {
+  var q28 = document.getElementById("q28more");
+  if (q28.style.display === "block") {
+    q28.style.display = "none";
+  } else {
+    q28.style.display = "block";
+  }
+}
+function q29function() {
+  var q29 = document.getElementById("q29more");
+  if (q29.style.display === "block") {
+    q29.style.display = "none";
+  } else {
+    q29.style.display = "block";
+  }
+}
+function q30function() {
+  var q30 = document.getElementById("q30more");
+  if (q30.style.display === "block") {
+    q30.style.display = "none";
+  } else {
+    q30.style.display = "block";
+  }
+}
+function q31function() {
+  var q31 = document.getElementById("q31more");
+  if (q31.style.display === "block") {
+    q31.style.display = "none";
+  } else {
+    q31.style.display = "block";
+  }
+}
+function q32function() {
+  var q32 = document.getElementById("q32more");
+  if (q32.style.display === "block") {
+    q32.style.display = "none";
+  } else {
+    q32.style.display = "block";
+  }
+}
+function q33function() {
+  var q33 = document.getElementById("q33more");
+  if (q33.style.display === "block") {
+    q33.style.display = "none";
+  } else {
+    q33.style.display = "block";
+  }
+}
+function q34function() {
+  var q34 = document.getElementById("q34more");
+  if (q34.style.display === "block") {
+    q34.style.display = "none";
+  } else {
+    q34.style.display = "block";
+  }
+}
+function q35function() {
+  var q35 = document.getElementById("q35more");
+  if (q35.style.display === "block") {
+    q35.style.display = "none";
+  } else {
+    q35.style.display = "block";
+  }
+}
+function q36function() {
+  var q36 = document.getElementById("q36more");
+  if (q36.style.display === "block") {
+    q36.style.display = "none";
+  } else {
+    q36.style.display = "block";
+  }
+}
+function q37function() {
+  var q37 = document.getElementById("q37more");
+  if (q37.style.display === "block") {
+    q37.style.display = "none";
+  } else {
+    q37.style.display = "block";
+  }
+}
+function q38function() {
+  var q38 = document.getElementById("q38more");
+  if (q38.style.display === "block") {
+    q38.style.display = "none";
+  } else {
+    q38.style.display = "block";
+  }
+}
+function q39function() {
+  var q39 = document.getElementById("q39more");
+  if (q39.style.display === "block") {
+    q39.style.display = "none";
+  } else {
+    q39.style.display = "block";
+  }
+}
+function q40function() {
+  var q40 = document.getElementById("q40more");
+  if (q40.style.display === "block") {
+    q40.style.display = "none";
+  } else {
+    q40.style.display = "block";
+  }
+}
+function q41function() {
+  var q41 = document.getElementById("q41more");
+  if (q41.style.display === "block") {
+    q41.style.display = "none";
+  } else {
+    q41.style.display = "block";
+  }
+}
+function q42function() {
+  var q42 = document.getElementById("q42more");
+  if (q42.style.display === "block") {
+    q42.style.display = "none";
+  } else {
+    q42.style.display = "block";
+  }
+}
+function q43function() {
+  var q43 = document.getElementById("q43more");
+  if (q43.style.display === "block") {
+    q43.style.display = "none";
+  } else {
+    q43.style.display = "block";
+  }
+}
+function q44function() {
+  var q44 = document.getElementById("q44more");
+  if (q44.style.display === "block") {
+    q44.style.display = "none";
+  } else {
+    q44.style.display = "block";
+  }
+}
+function q45function() {
+  var q45 = document.getElementById("q45more");
+  if (q45.style.display === "block") {
+    q45.style.display = "none";
+  } else {
+    q45.style.display = "block";
+  }
+}
+function q46function() {
+  var q46 = document.getElementById("q46more");
+  if (q46.style.display === "block") {
+    q46.style.display = "none";
+  } else {
+    q46.style.display = "block";
+  }
+}
+function q47function() {
+  var q47 = document.getElementById("q47more");
+  if (q47.style.display === "block") {
+    q47.style.display = "none";
+  } else {
+    q47.style.display = "block";
+  }
+}
+function q48function() {
+  var q48 = document.getElementById("q48more");
+  if (q48.style.display === "block") {
+    q48.style.display = "none";
+  } else {
+    q48.style.display = "block";
+  }
+}
+function q49function() {
+  var q49 = document.getElementById("q49more");
+  if (q49.style.display === "block") {
+    q49.style.display = "none";
+  } else {
+    q49.style.display = "block";
+  }
+}
+function q50function() {
+  var q50 = document.getElementById("q50more");
+  if (q50.style.display === "block") {
+    q50.style.display = "none";
+  } else {
+    q50.style.display = "block";
+  }
+}
+function q51function() {
+  var q51 = document.getElementById("q51more");
+  if (q51.style.display === "block") {
+    q51.style.display = "none";
+  } else {
+    q51.style.display = "block";
+  }
+}
+function q52function() {
+  var q52 = document.getElementById("q52more");
+  if (q52.style.display === "block") {
+    q52.style.display = "none";
+  } else {
+    q52.style.display = "block";
+  }
+}
+function q53function() {
+  var q53 = document.getElementById("q53more");
+  if (q53.style.display === "block") {
+    q53.style.display = "none";
+  } else {
+    q53.style.display = "block";
+  }
+}
+function q54function() {
+  var q54 = document.getElementById("q54more");
+  if (q54.style.display === "block") {
+    q54.style.display = "none";
+  } else {
+    q54.style.display = "block";
+  }
+}
+function q55function() {
+  var q55 = document.getElementById("q55more");
+  if (q55.style.display === "block") {
+    q55.style.display = "none";
+  } else {
+    q55.style.display = "block";
+  }
+}
+function q56function() {
+  var q56 = document.getElementById("q56more");
+  if (q56.style.display === "block") {
+    q56.style.display = "none";
+  } else {
+    q56.style.display = "block";
+  }
+}
+function q57function() {
+  var q57 = document.getElementById("q57more");
+  if (q57.style.display === "block") {
+    q57.style.display = "none";
+  } else {
+    q57.style.display = "block";
+  }
+}
+function q58function() {
+  var q58 = document.getElementById("q58more");
+  if (q58.style.display === "block") {
+    q58.style.display = "none";
+  } else {
+    q58.style.display = "block";
+  }
+}
+function q59function() {
+  var q59 = document.getElementById("q59more");
+  if (q59.style.display === "block") {
+    q59.style.display = "none";
+  } else {
+    q59.style.display = "block";
+  }
+}
+function q60function() {
+  var q60 = document.getElementById("q60more");
+  if (q60.style.display === "block") {
+    q60.style.display = "none";
+  } else {
+    q60.style.display = "block";
+  }
+}
+function q61function() {
+  var q61 = document.getElementById("q61more");
+  if (q61.style.display === "block") {
+    q61.style.display = "none";
+  } else {
+    q61.style.display = "block";
+  }
+}
+function q62function() {
+  var q62 = document.getElementById("q62more");
+  if (q62.style.display === "block") {
+    q62.style.display = "none";
+  } else {
+    q62.style.display = "block";
+  }
+}
+function q63function() {
+  var q63 = document.getElementById("q63more");
+  if (q63.style.display === "block") {
+    q63.style.display = "none";
+  } else {
+    q63.style.display = "block";
+  }
+}
+function q64function() {
+  var q64 = document.getElementById("q64more");
+  if (q64.style.display === "block") {
+    q64.style.display = "none";
+  } else {
+    q64.style.display = "block";
+  }
+}
+function q65function() {
+  var q65 = document.getElementById("q65more");
+  if (q65.style.display === "block") {
+    q65.style.display = "none";
+  } else {
+    q65.style.display = "block";
+  }
+}
+function q66function() {
+  var q66 = document.getElementById("q66more");
+  if (q66.style.display === "block") {
+    q66.style.display = "none";
+  } else {
+    q66.style.display = "block";
+  }
+}
+function q67function() {
+  var q67 = document.getElementById("q67more");
+  if (q67.style.display === "block") {
+    q67.style.display = "none";
+  } else {
+    q67.style.display = "block";
+  }
+}
+function q68function() {
+  var q68 = document.getElementById("q68more");
+  if (q68.style.display === "block") {
+    q68.style.display = "none";
+  } else {
+    q68.style.display = "block";
+  }
+}
+function q69function() {
+  var q69 = document.getElementById("q69more");
+  if (q69.style.display === "block") {
+    q69.style.display = "none";
+  } else {
+    q69.style.display = "block";
+  }
+}
+function q70function() {
+  var q70 = document.getElementById("q70more");
+  if (q70.style.display === "block") {
+    q70.style.display = "none";
+  } else {
+    q70.style.display = "block";
+  }
+}
